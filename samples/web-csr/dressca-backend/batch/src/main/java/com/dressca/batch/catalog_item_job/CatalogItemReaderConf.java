931a3cbe4d5a4e4bfe9d6ac2c645bc9fd880ad2e
大久保貴司 <takashi.ohkubo@biprogy.com>
2022-06-08 15:33:11 +0900

    バッチサンプル作成

---
+++ samples/web-csr/dressca-backend/batch/src/main/java/com/dressca/batch/catalog_item_job/CatalogItemReaderConf.java
@@ -0,0 +1,33 @@
+package com.dressca.batch.catalog_item_job;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.batch.MyBatisPagingItemReader;
+import org.mybatis.spring.batch.builder.MyBatisPagingItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.dressca.applicationcore.catalog.CatalogItem;
+
+/**
+ * CATALOG_ITEMテーブルからデータ一覧を取得するReaderの設定.
+ */
+@Configuration
+public class CatalogItemReaderConf {
+
+    @Autowired
+    SqlSessionFactory sqlSessionFactory;
+
+    /**
+     * CATALOG_ITEMテーブルからデータ一覧を取得するReader.
+     */
+    @Bean
+    public MyBatisPagingItemReader<CatalogItem> catalogItemReader() {
+        return new MyBatisPagingItemReaderBuilder<CatalogItem>()
+                .sqlSessionFactory(sqlSessionFactory)
+                .queryId("com.dressca.infrastructure.repository.mybatis.mapper.JoinedCatalogItemMapper.findAllForBatch")
+                .pageSize(10)
+                .build();
+    }
+
+}
