795aaec525bdb0543ce5385117bcfbda3c9b4adf
ykinoshita4 <yasuhiro.kinoshita@biprogy.com>
2023-10-02 14:48:54 +0900

    バッチのテストの出力ファイルの比較方法の修正

--- samples/web-csr/dressca-backend/batch/src/test/java/com/dressca/batch/CatalogItemJobTest.java
+++ samples/web-csr/dressca-backend/batch/src/test/java/com/dressca/batch/CatalogItemJobTest.java
@@ -76,7 +76,8 @@ public class CatalogItemJobTest {
     String expectedFile = EXPECTED_FOLDER + "output_jobTest_empty.csv";
     String outputStr = (new FileSystemResource(OUTPUT_FILE)).getContentAsString(Charset.forName("UTF-8"));
     String expectedStr = (new FileSystemResource(expectedFile)).getContentAsString(Charset.forName("UTF-8"));
-    assertThat(outputStr).isEqualTo(expectedStr);
+    // 期待値ファイルの改行コードは"\r\n"のため、出力ファイルの改行コード（OS依存）に変換して比較
+    assertThat(outputStr).isEqualTo(expectedStr.replaceAll("\r\n",System.getProperty("line.separator")));
   }
 
   /*
@@ -94,7 +95,8 @@ public class CatalogItemJobTest {
     String expectedFile = EXPECTED_FOLDER + "output_jobTest_10data.csv";
     String outputStr = (new FileSystemResource(OUTPUT_FILE)).getContentAsString(Charset.forName("UTF-8"));
     String expectedStr = (new FileSystemResource(expectedFile)).getContentAsString(Charset.forName("UTF-8"));
-    assertThat(outputStr).isEqualTo(expectedStr);
+    // 期待値ファイルの改行コードは"\r\n"のため、出力ファイルの改行コード（OS依存）に変換して比較
+    assertThat(outputStr).isEqualTo(expectedStr.replaceAll("\r\n",System.getProperty("line.separator")));
   }
 
 
@@ -113,7 +115,8 @@ public class CatalogItemJobTest {
     String expectedFile = EXPECTED_FOLDER + "output_stepTest_10data.csv";
     String outputStr = (new FileSystemResource(OUTPUT_FILE)).getContentAsString(Charset.forName("UTF-8"));
     String expectedStr = (new FileSystemResource(expectedFile)).getContentAsString(Charset.forName("UTF-8"));
-    assertThat(outputStr).isEqualTo(expectedStr);
+    // 期待値ファイルの改行コードは"\r\n"のため、出力ファイルの改行コード（OS依存）に変換して比較
+    assertThat(outputStr).isEqualTo(expectedStr.replaceAll("\r\n",System.getProperty("line.separator")));
   }
 
   private void insertTestData() {
