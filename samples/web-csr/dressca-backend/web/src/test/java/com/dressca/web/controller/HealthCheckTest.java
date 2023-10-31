1e96f35759c359f889003eee397d655a1721d759
RyoNakagawa <ryo.nakagawa2@biprogy.com>
2023-10-31 11:15:04 +0900

    プロパティの変更およびテスト用コードの追加

---
+++ samples/web-csr/dressca-backend/web/src/test/java/com/dressca/web/controller/HealthCheckTest.java
@@ -0,0 +1,51 @@
+package com.dressca.web.controller;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.dressca.web.WebApplication;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = WebApplication.class)
+@AutoConfigureMockMvc
+public class HealthCheckTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("testGet_03_ヘルスチェック_サーバ正常動作確認")
+    void testGet_serverCheck() {
+      try {
+        this.mockMvc.perform(get("/api/health/check"))
+          .andExpect(status().isOk())
+          .andExpect(content().json("{'status':'UP'}"));
+      } catch (Exception e) {
+        e.printStackTrace();
+        fail();
+      }
+    }
+  
+    @Test
+    @DisplayName("testGet_04_ヘルスチェック_DB正常動作確認")
+    void testGet_databaseCheck() {
+      try {
+        this.mockMvc.perform(get("/api/health/datasource"))
+          .andExpect(status().isOk())
+          .andExpect(content().json("{'status':'UP'}"));
+      } catch (Exception e) {
+        e.printStackTrace();
+        fail();
+      }
+    }
+}
