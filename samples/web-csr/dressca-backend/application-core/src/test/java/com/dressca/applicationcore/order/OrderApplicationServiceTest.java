15a3d336ac148b8bacd813df4acc6f2eff8ba1ac
RyoNakagawa <ryo.nakagawa2@biprogy.com>
2023-11-10 14:36:04 +0900

    fail()のインポート文を削除

--- samples/web-csr/dressca-backend/application-core/src/test/java/com/dressca/applicationcore/order/OrderApplicationServiceTest.java
+++ samples/web-csr/dressca-backend/application-core/src/test/java/com/dressca/applicationcore/order/OrderApplicationServiceTest.java
@@ -2,7 +2,6 @@ package com.dressca.applicationcore.order;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
