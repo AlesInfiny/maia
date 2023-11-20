e452addf6686e24fe9b2bd4d27811f1f3202f310
RyoNakagawa <ryo.nakagawa2@biprogy.com>
2023-11-20 14:02:20 +0900

    vscode上のlong型、Long型に対する警告への対応の修正

--- samples/web-csr/dressca-backend/application-core/src/test/java/com/dressca/applicationcore/catalog/CatalogDomainServiceTest.java
+++ samples/web-csr/dressca-backend/application-core/src/test/java/com/dressca/applicationcore/catalog/CatalogDomainServiceTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -28,22 +29,25 @@ public class CatalogDomainServiceTest {
   @Test
   void testGetExistCatalogItems_正常系_リポジトリのfindByCategoryIdInを1度だけ呼出す() {
     // Arrange
-    List<Long> catalogItemIds = List.of(1L, 2L);
-    List<CatalogItem> catalogItems = catalogItemIds.stream().map(this::createCatalogItem).collect(Collectors.toList());
-    when(this.repository.findByCatalogItemIdIn(catalogItemIds)).thenReturn(catalogItems);
+    long[] catalogItemIds = { 1L, 2L };
+    List<Long> catalogItemIdsList = Arrays.asList(Arrays.stream(catalogItemIds).boxed().toArray(Long[]::new));
+    List<CatalogItem> catalogItems = Arrays.stream(catalogItemIds).mapToObj(this::createCatalogItem)
+        .collect(Collectors.toList());
+    when(this.repository.findByCatalogItemIdIn(catalogItemIdsList)).thenReturn(catalogItems);
 
     // Act
-    service.getExistCatalogItems(catalogItemIds);
+    service.getExistCatalogItems(catalogItemIdsList);
 
     // Assert
-    verify(this.repository, times(1)).findByCatalogItemIdIn(catalogItemIds);
+    verify(this.repository, times(1)).findByCatalogItemIdIn(catalogItemIdsList);
   }
 
   @Test
   void testExistAll_正常系_リポジトリ内に存在するアイテムのリストを返す() {
     // Arrange
-    List<Long> catalogItemIds = List.of(2L);
-    List<CatalogItem> catalogItems = catalogItemIds.stream().map(this::createCatalogItem).collect(Collectors.toList());
+    long[] catalogItemIds = { 2L };
+    List<CatalogItem> catalogItems = Arrays.stream(catalogItemIds).mapToObj(this::createCatalogItem)
+        .collect(Collectors.toList());
     when(this.repository.findByCatalogItemIdIn(List.of(1L, 2L))).thenReturn(catalogItems);
 
     // Act
@@ -57,23 +61,27 @@ public class CatalogDomainServiceTest {
   @Test
   void testExistAll_正常系_リポジトリのfindByCategoryIdInを1度だけ呼出す() {
     // Arrange
-    List<Long> catalogItemIds = List.of(1L, 2L);
-    List<CatalogItem> catalogItems = catalogItemIds.stream().map(this::createCatalogItem).collect(Collectors.toList());
-    when(this.repository.findByCatalogItemIdIn(catalogItemIds)).thenReturn(catalogItems);
+    long[] catalogItemIds = { 1L, 2L };
+    List<Long> catalogItemIdsList = Arrays.asList(Arrays.stream(catalogItemIds).boxed().toArray(Long[]::new));
+    List<CatalogItem> catalogItems = Arrays.stream(catalogItemIds).mapToObj(this::createCatalogItem)
+        .collect(Collectors.toList());
+    when(this.repository.findByCatalogItemIdIn(catalogItemIdsList)).thenReturn(catalogItems);
 
     // Act
-    service.existAll(catalogItemIds);
+    service.existAll(catalogItemIdsList);
 
     // Assert
-    verify(this.repository, times(1)).findByCatalogItemIdIn(catalogItemIds);
+    verify(this.repository, times(1)).findByCatalogItemIdIn(catalogItemIdsList);
   }
 
   @Test
   void testExistAll_正常系_カタログアイテムIdがすべて存在する場合trueを返す() {
     // Arrange
-    List<Long> catalogItemIds = List.of(1L, 2L);
-    List<CatalogItem> catalogItems = catalogItemIds.stream().map(this::createCatalogItem).collect(Collectors.toList());
-    when(this.repository.findByCatalogItemIdIn(catalogItemIds)).thenReturn(catalogItems);
+    long[] catalogItemIds = { 1L, 2L };
+    List<Long> catalogItemIdsList = Arrays.asList(Arrays.stream(catalogItemIds).boxed().toArray(Long[]::new));
+    List<CatalogItem> catalogItems = Arrays.stream(catalogItemIds).mapToObj(this::createCatalogItem)
+        .collect(Collectors.toList());
+    when(this.repository.findByCatalogItemIdIn(catalogItemIdsList)).thenReturn(catalogItems);
 
     // Act
     boolean existAll = service.existAll(List.of(1L, 2L));
@@ -85,8 +93,9 @@ public class CatalogDomainServiceTest {
   @Test
   void testExistAll_正常系_カタログアイテムIdが一部だけ存在する場合falseを返す() {
     // Arrange
-    List<Long> catalogItemIds = List.of(2L);
-    List<CatalogItem> catalogItems = catalogItemIds.stream().map(this::createCatalogItem).collect(Collectors.toList());
+    long[] catalogItemIds = {};
+    List<CatalogItem> catalogItems = Arrays.stream(catalogItemIds).mapToObj(this::createCatalogItem)
+        .collect(Collectors.toList());
     when(this.repository.findByCatalogItemIdIn(List.of(1L, 2L))).thenReturn(catalogItems);
 
     // Act
@@ -99,8 +108,9 @@ public class CatalogDomainServiceTest {
   @Test
   void testExistAll_正常系_カタログアイテムIdが1件も存在しない場合falseを返す() {
     // Arrange
-    List<Long> catalogItemIds = List.of();
-    List<CatalogItem> catalogItems = catalogItemIds.stream().map(this::createCatalogItem).collect(Collectors.toList());
+    long[] catalogItemIds = {};
+    List<CatalogItem> catalogItems = Arrays.stream(catalogItemIds).mapToObj(this::createCatalogItem)
+        .collect(Collectors.toList());
     when(this.repository.findByCatalogItemIdIn(List.of(1L, 2L))).thenReturn(catalogItems);
 
     // Act
@@ -110,7 +120,7 @@ public class CatalogDomainServiceTest {
     assertThat(existAll).isFalse();
   }
 
-  private CatalogItem createCatalogItem(Long id) {
+  private CatalogItem createCatalogItem(long id) {
     Random random = new Random();
     long defaultCatalogCategoryId = random.nextInt(1000);
     long defaultCatalogBrandId = random.nextInt(1000);
