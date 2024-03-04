1d76fa34f997ad6c59b45d37bb3fe99e46bbbd5d
RyoNakagawa <ryo.nakagawa2@biprogy.com>
2024-03-04 14:22:42 +0900

    backend側の不要なプログラムの削除およびそれに伴うプログラムの修正

--- samples/azure-ad-b2c-sample/auth-backend/system-common/src/main/java/com/dressca/systemcommon/constant/ExceptionIdConstant.java
+++ samples/azure-ad-b2c-sample/auth-backend/system-common/src/main/java/com/dressca/systemcommon/constant/ExceptionIdConstant.java
@@ -6,17 +6,4 @@ package com.dressca.systemcommon.constant;
 public class ExceptionIdConstant {
   /** 想定外のエラー。 */
   public static final String E_SHARE0000 = "E_SHARE0000";
-
-  // 業務例外用
-  /** アセットが見つからなかった際の例外。 */
-  public static final String E_ASSET0001 = "E_ASSET0001";
-
-  /** 買い物かごが見つからなかった際の例外。 */
-  public static final String E_BASKET0001 = "E_BASKET0001";
-
-  /** 注文のチェックアウト処理開始時に買い物かごが空だった際の例外。 */
-  public static final String E_ORDER0001 = "E_ORDER0001";
-
-  /** 注文情報が存在しなかった際の例外。 */
-  public static final String E_ORDER0002 = "E_ORDER0002";
 }
--- samples/azure-ad-b2c-sample/auth-backend/system-common/src/main/java/com/dressca/systemcommon/constant/MessageIdConstant.java
+++ samples/azure-ad-b2c-sample/auth-backend/system-common/src/main/java/com/dressca/systemcommon/constant/MessageIdConstant.java
@@ -9,21 +9,4 @@ public class MessageIdConstant {
   /** 想定外のエラーが発生しました。 */
   public static final String E_SHARE0000_FRONT = "F_SHARE0000.front";
   public static final String E_SHARE0000_LOG = "F_SHARE0000.log";
-
-  // 業務エラーメッセージ用
-  /** アセットコード: {0} のアセットが見つかりませんでした。 */
-  public static final String E_ASSET0001_FRONT = "E_ASSET0001.front";
-  public static final String E_ASSET0001_LOG = "E_ASSET0001.log";
-
-  /** 買い物かごID: {0} の買い物かごが見つかりませんでした。 */
-  public static final String E_BASKET0001_FRONT = "E_BASKET0001.front";
-  public static final String E_BASKET0001_LOG = "E_BASKET0001.log";
-
-  /** 注文のチェックアウト時に買い物かごが空でした。 */
-  public static final String E_ORDER0001_FRONT = "E_ORDER0001.front";
-  public static final String E_ORDER0001_LOG = "E_ORDER0001.log";
-
-  /** 注文ID: {0}, 購入者ID: {1} に該当する注文情報が見つかりませんでした。 */
-  public static final String E_ORDER0002_FRONT = "E_ORDER0002.front";
-  public static final String E_ORDER0002_LOG = "E_ORDER0002.log";
 }
