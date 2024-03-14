a342207e6518efc96c26faf1f801fdd79f1be2ea
Kaori ISHIKAWA <kaori.ishikawa@biprogy.com>
2024-03-14 16:32:13 +0900

    VITE_API_PREFIXを削除

--- samples/web-csr/dressca-frontend/env.d.ts
+++ samples/web-csr/dressca-frontend/env.d.ts
@@ -3,7 +3,6 @@ interface ImportMetaEnv {
   readonly VITE_NO_ASSET_URL: string;
   readonly VITE_ASSET_URL: string;
   readonly VITE_BACKEND_ENDPOINT_ORIGIN: string;
-  readonly VITE_API_PREFIX: string;
 }
 
 interface ImportMeta {
