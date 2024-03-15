a52d4dae9f5960964152339e38076f5f0e81c62b
Yamada Shumpei <shumpei.yamada@biplogy.com>
2024-03-15 18:36:56 +0900

    Webに記載されていたMyBatis関連の設定をInfrastructureに移動

--- samples/web-csr/dressca-backend/web/src/main/java/com/dressca/web/WebApplication.java
+++ samples/web-csr/dressca-backend/web/src/main/java/com/dressca/web/WebApplication.java
@@ -1,6 +1,5 @@
 package com.dressca.web;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,7 +12,6 @@ import io.swagger.v3.oas.annotations.info.Info;
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Dressca", description = "ECサイトDressca", version = "v1"))
 @ComponentScan(basePackages = { "com.dressca" })
-@MapperScan(basePackages = { "com.dressca.infrastructure.repository.mybatis" })
 public class WebApplication {
 
   public static void main(String[] args) {
