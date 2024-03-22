b331fc9df45e4a14b518906134e50b4bafaaef7f
RyoNakagawa <ryo.nakagawa2@biprogy.com>
2024-03-22 10:15:42 +0900

    利用していないimport文の削除

--- samples/azure-ad-b2c-sample/auth-backend/web/src/main/java/com/dressca/web/controller/UserController.java
+++ samples/azure-ad-b2c-sample/auth-backend/web/src/main/java/com/dressca/web/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.dressca.web.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
