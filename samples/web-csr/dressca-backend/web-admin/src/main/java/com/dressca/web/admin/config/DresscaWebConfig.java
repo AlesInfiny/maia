38a7890a8ca6efa933d97700d8bac7a40e7e38af
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2024-09-19 11:00:06 +0900

    web-adminから不要なファイルを削除

--- samples/web-csr/dressca-backend/web-admin/src/main/java/com/dressca/web/admin/config/DresscaWebConfig.java
+++ samples/web-csr/dressca-backend/web-admin/src/main/java/com/dressca/web/admin/config/DresscaWebConfig.java
@@ -1,9 +1,5 @@
 package com.dressca.web.admin.config;
 
-import com.dressca.web.admin.filter.BuyerIdFilter;
-import jakarta.servlet.Filter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -12,15 +8,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DresscaWebConfig {
 
-  /**
-   * BuyerIdFilter の設定。
-   * 
-   * @return BuyerIdFilter
-   */
-  @Bean
-  public FilterRegistrationBean<Filter> buyerIdFilter() {
-    FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>();
-    bean.setFilter(new BuyerIdFilter());
-    return bean;
-  }
 }
