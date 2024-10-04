5c4c7f36d219651de82a4d351be86fe64c0d4047
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-10-04 15:37:12 +0900

    AzureADB2Cサンプルのクライアントコードを移植

--- samples/azure-ad-b2c-sample/auth-frontend/src/api-client/index.ts
+++ samples/azure-ad-b2c-sample/auth-frontend/src/api-client/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import * as apiClient from '@/generated/api-client';
-import { useAuthenticationStore } from '@/stores/authentication/authentication';
+import { authenticationService } from '@/services/authentication/authentication-service';
 
 /** axios の共通の設定があればここに定義します。 */
 const axiosInstance = axios.create({
@@ -18,22 +18,20 @@ function createConfig(): apiClient.Configuration {
 }
 
 async function addTokenAsync(config: apiClient.Configuration): Promise<void> {
-  const store = useAuthenticationStore();
-
   // 認証済みの場合、アクセストークンを取得して Configuration に設定します。
-  if (store.isAuthenticated) {
-    await store.getToken();
-    const token = store.getAccessToken;
+  if (await authenticationService.isAuthenticated()) {
+    const token = await authenticationService.getTokenAzureADB2C();
+    // eslint-disable-next-line no-param-reassign
     config.accessToken = token;
   }
 }
 
-export async function getUserApi(): Promise<apiClient.UserApi> {
+export async function getUsersApi(): Promise<apiClient.UsersApi> {
   const config = createConfig();
 
   // UsersApi は認証が必要な API なので、addTokenAsync を呼び出します。
   await addTokenAsync(config);
-  const userApi = new apiClient.UserApi(config, '', axiosInstance);
+  const userApi = new apiClient.UsersApi(config, '', axiosInstance);
   return userApi;
 }
 
