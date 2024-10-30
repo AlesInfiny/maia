87f2293158a68e7e53f6cce75d1e262e1442a0ea
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-10-30 16:16:14 +0900

    エントリーポイントでuseする

--- samples/azure-ad-b2c-sample/auth-frontend/src/main.ts
+++ samples/azure-ad-b2c-sample/auth-frontend/src/main.ts
@@ -2,11 +2,13 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { globalErrorHandler } from '@/shared/error-handler/global-error-handler';
 import App from './App.vue';
+import { createCustomErrorHandler } from './shared/error-handler/custom-error-handler';
 
 const app = createApp(App);
 
 app.use(createPinia());
 
 app.use(globalErrorHandler);
+app.use(createCustomErrorHandler());
 
 app.mount('#app');
