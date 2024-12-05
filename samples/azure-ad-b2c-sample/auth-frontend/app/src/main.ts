bc134254eef73c924d9552fecf5fe07f03bf32d0
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-12-05 17:32:46 +0900

    mswの雛形を導入

--- samples/azure-ad-b2c-sample/auth-frontend/app/src/main.ts
+++ samples/azure-ad-b2c-sample/auth-frontend/app/src/main.ts
@@ -2,6 +2,22 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 
+async function enableMocking(): Promise<ServiceWorkerRegistration | undefined> {
+  const { worker } = await import('../mock/browser');
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+  });
+}
+
+if (import.meta.env.MODE === 'mock') {
+  try {
+    await enableMocking();
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('モック用のワーカープロセスの起動に失敗しました。', error);
+  }
+}
+
 const app = createApp(App);
 
 app.use(createPinia());
