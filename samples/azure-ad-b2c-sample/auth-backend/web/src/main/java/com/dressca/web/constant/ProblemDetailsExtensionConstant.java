2421187c3cddcea2a0faca018032088d3fb199ce
RyoNakagawa <ryo.nakagawa2@biprogy.com>
2024-12-19 10:47:54 +0900

    メッセージ管理機能をADB2Cプロジェクトに導入

---
+++ samples/azure-ad-b2c-sample/auth-backend/web/src/main/java/com/dressca/web/constant/ProblemDetailsExtensionConstant.java
@@ -0,0 +1,13 @@
+package com.dressca.web.constant;
+
+/**
+ * ProblemDetailの拡張メンバー用の定数クラス。
+ */
+public class ProblemDetailsExtensionConstant {
+
+  /** Exception ID。 */
+  public static final String EXCEPTION_ID = "exceptionId";
+
+  /** Exception ID に紐づく例外値。 */
+  public static final String EXCEPTION_VALUES = "exceptionValues";
+}
