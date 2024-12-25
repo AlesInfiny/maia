a9a2f7c9f6ff3341ae6249381249bf336435cadd
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2024-12-25 11:05:46 +0900

    DisplayNameのimport文を追記

--- samples/web-csr/dressca-backend/web-admin/src/test/java/com/dressca/web/admin/WebApplicationTests.java
+++ samples/web-csr/dressca-backend/web-admin/src/test/java/com/dressca/web/admin/WebApplicationTests.java
@@ -2,6 +2,7 @@ package com.dressca.web.admin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.DisplayName;
 
 @SpringBootTest
 class WebApplicationTests {
