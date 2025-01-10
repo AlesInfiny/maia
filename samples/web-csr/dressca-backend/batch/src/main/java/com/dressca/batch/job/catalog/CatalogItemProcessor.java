53894f2691ab14e316bf7199ccc61e604d66cd91
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2025-01-10 17:17:07 +0900

    バッチのJavaDocを追記

--- samples/web-csr/dressca-backend/batch/src/main/java/com/dressca/batch/job/catalog/CatalogItemProcessor.java
+++ samples/web-csr/dressca-backend/batch/src/main/java/com/dressca/batch/job/catalog/CatalogItemProcessor.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 import com.dressca.applicationcore.catalog.CatalogItem;
 
 /**
- * CatalogItemの商品名をバッチ出力用に先頭10文字にするprocessor。
+ * {@link CatalogItem} の商品名をバッチ出力用に先頭 10 文字にする processor です。
  */
 @Component
 public class CatalogItemProcessor implements ItemProcessor<CatalogItem, CatalogItem> {
@@ -15,7 +15,7 @@ public class CatalogItemProcessor implements ItemProcessor<CatalogItem, CatalogI
   @Override
   public CatalogItem process(@NonNull final CatalogItem catalogItem) throws Exception {
     String name = catalogItem.getName();
-    // 商品名が10文字超える場合、先頭10文字にする
+    // 商品名が 10 文字超える場合、先頭 10 文字にする
     if (name.length() > 10) {
       name = name.substring(0, 10);
     }
