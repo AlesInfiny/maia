18b4f2150710804350b1d8ee8f77701a770f2bce
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2025-02-04 16:14:11 +0900

    H2をサーバーモードで起動するクラスを修正

--- samples/web-csr/dressca-backend/web-consumer/src/main/java/com/dressca/web/consumer/config/H2ServerLauncher.java
+++ samples/web-csr/dressca-backend/web-consumer/src/main/java/com/dressca/web/consumer/config/H2ServerLauncher.java
@@ -1,35 +1,37 @@
 package com.dressca.web.consumer.config;
 
 import org.h2.tools.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import jakarta.annotation.PostConstruct;
+import com.dressca.systemcommon.constant.SystemPropertyConstants;
 import jakarta.annotation.PreDestroy;
 import java.sql.SQLException;
 
 /**
- * 開発環境で h2 データベースを立ち上げるためのクラスです。
+ * 開発環境で H2 Database をサーバーモードで立ち上げるためのクラスです。
  */
 @Component
 @Profile("local")
 public class H2ServerLauncher {
 
   private Server tcpServer;
+  private final Logger apLog = LoggerFactory.getLogger(SystemPropertyConstants.APPLICATION_LOG_LOGGER);
 
   /**
-   * h2 サーバをサーバーモードで起動します。
+   * {@link H2ServerLauncher} クラスのインスタンスを初期化し、 H2 Database をサーバーモードで起動します。
    */
-  @PostConstruct
-  public void start() {
+  public H2ServerLauncher() {
     try {
       this.tcpServer = Server.createTcpServer("-tcpPort", "9092", "-tcpAllowOthers", "-ifNotExists").start();
     } catch (SQLException e) {
-      System.out.println("H2 server is already started.");
+      apLog.info("H2 Database は既にサーバーモードで起動しています。");
     }
   }
 
   /**
-   * h2 サーバを停止します。
+   * インスタンスを破棄する際に H2 Database を停止します。
    */
   @PreDestroy
   public void stop() {
