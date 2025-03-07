fe26694964dbf1eb0b4a302532f494da654f6160
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2025-03-07 14:16:07 +0900

    スピナーコンポーネントを背景色を持つように修正

--- samples/web-csr/dressca-frontend/admin/src/components/common/LoadingSpinnerOverlay/LoadingSpinnerOverlay.vue
+++ samples/web-csr/dressca-frontend/admin/src/components/common/LoadingSpinnerOverlay/LoadingSpinnerOverlay.vue
@@ -28,13 +28,14 @@ defineSlots<{
 
 <style scoped>
 .overlay {
-  position: fixed;
-  inset: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   z-index: 50;
   overflow: hidden;
-  background-color: transparent;
+  background-color: white;
   display: flex;
   align-items: center;
   justify-content: center;
