ef5092ee6890a90352e948ceaa3beb264c56785a
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2025-03-31 15:24:24 +0900

    consumerの各ルーティング定義にrequiresAuthプロパティを追加

--- samples/web-csr/dressca-frontend/consumer/src/router/ordering/ordering.ts
+++ samples/web-csr/dressca-frontend/consumer/src/router/ordering/ordering.ts
@@ -5,16 +5,19 @@ export const orderingRoutes: RouteRecordRaw[] = [
     path: '/ordering/checkout',
     name: 'ordering/checkout',
     component: () => import('@/views/ordering/CheckoutView.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: '/ordering/done',
     name: 'ordering/done',
     component: () => import('@/views/ordering/DoneView.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: '/ordering/done/:orderId(\\d+)',
     name: 'ordering/done',
     component: () => import('@/views/ordering/DoneView.vue'),
+    meta: { requiresAuth: true },
     props: (route) => ({
       orderId: Number(route.params.orderId),
     }),
