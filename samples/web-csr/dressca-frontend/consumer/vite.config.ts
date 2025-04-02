9dda2b2beaacbb349d49d0077a1418a259b2ee20
RyoNakagawa <ryo.nakagawa2@biprogy.com>
2025-04-02 14:00:29 +0900

    検証作業用のフロントエンドの変更を修正

--- samples/web-csr/dressca-frontend/consumer/vite.config.ts
+++ samples/web-csr/dressca-frontend/consumer/vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
-      port: 5174,
+      port: 5173,
       proxy: {
         '/api': {
           target: env.VITE_PROXY_ENDPOINT_ORIGIN,
