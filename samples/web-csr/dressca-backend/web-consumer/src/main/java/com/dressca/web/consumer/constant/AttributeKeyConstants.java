b2fb66e9c679cfccf762d1e4a6fcf7b44caa178c
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2025-05-12 14:06:25 +0900

    属性キーを管理するクラスを追加

---
+++ samples/web-csr/dressca-backend/web-consumer/src/main/java/com/dressca/web/consumer/constant/AttributeKeyConstants.java
@@ -0,0 +1,11 @@
+package com.dressca.web.consumer.constant;
+
+/**
+ * 属性キーを管理する定数クラスです。
+ */
+public class AttributeKeyConstants {
+
+  /** 購入者 ID の属性キーです。 */
+  public static final String BUYER_ID_ATTRIBUTE_KEY = "buyerId";
+
+}
