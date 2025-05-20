7ba8579e08aff05015f711d43ed3ba6511eb8ba4
RyoNakagawa <ryo.nakagawa2@biprogy.com>
2025-05-20 19:04:05 +0900

    雛形作成

---
+++ samples/dressca-cms/mpa/src/main/java/com/dressca/mpa/MpaApplication.java
@@ -0,0 +1,12 @@
+package com.dressca.mpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpaApplication.class, args);
+	}
+}
