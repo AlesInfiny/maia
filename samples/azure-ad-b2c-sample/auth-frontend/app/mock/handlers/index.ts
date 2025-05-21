e6b09acd38f4237ba0aaa19cb30f2870e4fc03b4
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-05-21 17:17:58 +0900

    動作確認用の handler を実装

--- samples/azure-ad-b2c-sample/auth-frontend/app/mock/handlers/index.ts
+++ samples/azure-ad-b2c-sample/auth-frontend/app/mock/handlers/index.ts
@@ -1,1 +1,15 @@
-export const handlers = [];
+import { http, HttpResponse } from 'msw';
+
+export const handlers = [
+  http.get('/api/servertime', () => {
+    return HttpResponse.json({
+      serverTime: new Date().toISOString(),
+    });
+  }),
+
+  http.get('/api/users', () => {
+    return HttpResponse.json({
+      userId: 'mock-user-id-123',
+    });
+  }),
+];
