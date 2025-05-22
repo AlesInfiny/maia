9871b84cf09caf514d58ae922330ea9a7ab99aeb
RyoNakagawa <ryo.nakagawa2@biprogy.com>
2025-05-22 18:04:57 +0900

    MPAサンプルの作成

--- samples/dressca-cms/mpa/src/main/java/com/dressca/mpa/controller/LoginController.java
+++ samples/dressca-cms/mpa/src/main/java/com/dressca/mpa/controller/LoginController.java
@@ -4,15 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
-
-  @GetMapping("/index")
+public class LoginController {
+  
+  @GetMapping("/login")
 	String home() {
-		return "index";
-	}
-
-	@GetMapping("/settings")
-	String settings() {
-		return "settings";
+		return "login";
 	}
 }
