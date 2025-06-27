d2a20760bd8be4bad9293b7e7de02e1ad2e044bf
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-06-27 21:16:29 +0900

    AzureADB2Cサンプルに vite-plugin-vue-devtools を導入

--- samples/azure-ad-b2c-sample/auth-frontend/app/vite.config.ts
+++ samples/azure-ad-b2c-sample/auth-frontend/app/vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const plugins = [vue(), vueJsx()];
+  const plugins = [vue(), vueJsx(), vueDevTools()];
   const env = loadEnv(mode, process.cwd());
 
   return {
