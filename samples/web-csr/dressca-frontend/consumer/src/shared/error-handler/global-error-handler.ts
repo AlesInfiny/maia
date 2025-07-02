a11acadcc1e9218bd23b556256f131b190faa214
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-07-02 14:32:26 +0900

    consumer にフォーマットを適用

--- samples/web-csr/dressca-frontend/consumer/src/shared/error-handler/global-error-handler.ts
+++ samples/web-csr/dressca-frontend/consumer/src/shared/error-handler/global-error-handler.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
-import type { App, ComponentPublicInstance } from 'vue';
-import { router } from '../../router';
+import type { App, ComponentPublicInstance } from 'vue'
+import { router } from '../../router'
 
 export const globalErrorHandler = {
   /* eslint no-param-reassign: 0 */
@@ -12,19 +12,19 @@ export const globalErrorHandler = {
     ) => {
       // 本サンプルAPではログの出力とエラー画面への遷移を行っています。
       // APの要件によってはサーバーやログ収集ツールにログを送信し、エラーを握りつぶすこともあります。
-      console.error(err, instance, info);
-      router.replace({ name: 'error' });
-    };
+      console.error(err, instance, info)
+      router.replace({ name: 'error' })
+    }
 
     // Vue.js 以外のエラー
     // テストやデバッグ時にエラーの発生を検知するために利用する
     window.addEventListener('error', (event) => {
-      console.error(event);
-    });
+      console.error(event)
+    })
 
     // テストやデバッグ時に予期せぬ非同期エラーの発生を検知するために利用する
     window.addEventListener('unhandledrejection', (event) => {
-      console.error(event);
-    });
+      console.error(event)
+    })
   },
-};
+}
