d5d3ff546a8af0898f00382a30d4892eb309275a
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-07-02 11:30:03 +0900

    コードフォーマットを実行

--- samples/azure-ad-b2c-sample/auth-frontend/app/src/shared/error-handler/global-error-handler.ts
+++ samples/azure-ad-b2c-sample/auth-frontend/app/src/shared/error-handler/global-error-handler.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import type { App, ComponentPublicInstance } from 'vue';
+import type { App, ComponentPublicInstance } from 'vue'
 
 export const globalErrorHandler = {
   /* eslint no-param-reassign: 0 */
@@ -11,18 +11,18 @@ export const globalErrorHandler = {
     ) => {
       // 本サンプルAPではコンソールへログの出力を行います。
       // APの要件によってはサーバーやログ収集ツールにログを送信し、エラーを握りつぶすこともあります。
-      console.error(err, instance, info);
-    };
+      console.error(err, instance, info)
+    }
 
     // Vue.js 以外のエラー
     // テストやデバッグ時にエラーの発生を検知するために利用します。
     window.addEventListener('error', (event) => {
-      console.error(event);
-    });
+      console.error(event)
+    })
 
     // テストやデバッグ時に予期せぬ非同期エラーの発生を検知するために利用します。
     window.addEventListener('unhandledrejection', (event) => {
-      console.error(event);
-    });
+      console.error(event)
+    })
   },
-};
+}
