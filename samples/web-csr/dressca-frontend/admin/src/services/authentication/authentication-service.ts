40d25e8591211b25d598ca3dd2c427bcdac388c6
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-07-02 14:30:40 +0900

    admin にフォーマットを実行

--- samples/web-csr/dressca-frontend/admin/src/services/authentication/authentication-service.ts
+++ samples/web-csr/dressca-frontend/admin/src/services/authentication/authentication-service.ts
@@ -1,17 +1,17 @@
-import { useAuthenticationStore } from '@/stores/authentication/authentication';
+import { useAuthenticationStore } from '@/stores/authentication/authentication'
 
 /**
  * アプリケーションにログインします。
  */
 export async function loginAsync() {
-  const authenticationStore = useAuthenticationStore();
-  await authenticationStore.signInAsync();
+  const authenticationStore = useAuthenticationStore()
+  await authenticationStore.signInAsync()
 }
 
 /**
  * アプリケーションからログアウトします。
  */
 export async function logoutAsync() {
-  const authenticationStore = useAuthenticationStore();
-  await authenticationStore.signOutAsync();
+  const authenticationStore = useAuthenticationStore()
+  await authenticationStore.signOutAsync()
 }
