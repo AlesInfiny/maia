5710e01421b12cd85e603e53ecc23de6066960d2
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-07-03 14:00:23 +0900

    [Dressca]フォーマット関連の設定をcreate vue3.17.0準拠に更新 (#2708)
    * admin/consumer のフォーマット設定を最新化
    
    * admin にフォーマットを実行
    
    * consumer にフォーマットを適用
    
    * コマンドの相対パスをeslintと統一

--- samples/web-csr/dressca-frontend/consumer/src/stores/authentication/authentication.ts
+++ samples/web-csr/dressca-frontend/consumer/src/stores/authentication/authentication.ts
@@ -1,4 +1,4 @@
-import { defineStore } from 'pinia';
+import { defineStore } from 'pinia'
 
 export const useAuthenticationStore = defineStore('authentication', {
   state: () => ({
@@ -8,16 +8,13 @@ export const useAuthenticationStore = defineStore('authentication', {
   }),
   actions: {
     async signInAsync() {
-      this.authenticationState = true;
-      sessionStorage.setItem(
-        'isAuthenticated',
-        JSON.stringify(this.authenticationState),
-      );
+      this.authenticationState = true
+      sessionStorage.setItem('isAuthenticated', JSON.stringify(this.authenticationState))
     },
   },
   getters: {
     isAuthenticated(state) {
-      return state.authenticationState;
+      return state.authenticationState
     },
   },
-});
+})
