fd352bcad237d4e85392b3a9283bbda3291a24f7
RyoNakagawa <ryo.nakagawa2@biprogy.com>
2025-07-17 17:33:02 +0900

    メッセージ管理機能の導入

--- samples/dressca-cms/mpa/src/main/java/com/dressca/mpa/config/I18nConfig.java
+++ samples/dressca-cms/mpa/src/main/java/com/dressca/mpa/config/I18nConfig.java
@@ -23,24 +23,13 @@ public class I18nConfig {
         // ── i18n 以下をスキャン ──
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         // /i18n/ja/messages.properties や /i18n/en/messages.properties を拾う
-        Resource[] resources = resolver.getResources("classpath:/i18n/*/messages.properties");
+        Resource[] resources = resolver.getResources("classpath:/i18n/**/*.properties");
 
-        // フォルダ名（ja, en, ...） を取り出して baseNames に整形
         String[] baseNames = Arrays.stream(resources)
-            .map(resource -> {
-                try {
-                    return resource.getURI();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            })
-            .map(uri -> {
-                // URI: .../i18n/ja/messages.properties → extract "i18n/ja/messages"
-                String path = uri.getPath();
-                return path
-                    .replaceFirst(".*(/i18n/.+)\\.properties$", "$1");
-            })
-            .map(s -> "classpath:" + s)
+            .map(Resource::getFilename)
+            .map(fn -> fn.replaceAll("(_[a-z]{2})?\\.properties$", ""))
+            .distinct()
+            .map(bn -> "classpath:/i18n/" + bn)
             .toArray(String[]::new);
 
         ms.setBasenames(baseNames);
