21d174f61459884baa15180ed02eeda06604645c
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2025-09-01 17:16:39 +0900

    バリデーションを強化し、AnnouncementContentViewModelのタイトルとメッセージフィールドに@NotBlankアノテーションを追加。create.htmlにエラーメッセージ表示用のdivを追加し、index.htmlで掲載終了日時の表示ロジックを修正。

--- samples/cms/web/src/main/java/com/dressca/cms/web/models/base/AnnouncementContentViewModel.java
+++ samples/cms/web/src/main/java/com/dressca/cms/web/models/base/AnnouncementContentViewModel.java
@@ -2,6 +2,7 @@ package com.dressca.cms.web.models.base;
 
 import org.hibernate.validator.constraints.URL;
 import com.dressca.cms.web.models.validation.AnnouncementValidationGroup.AnnouncementStoreGroup;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -15,25 +16,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AnnouncementContentViewModel {
-  @NotNull(message = "言語を選択してください", groups = AnnouncementStoreGroup.class)
-  private String languageCode;
+    @NotNull(message = "言語を選択してください", groups = AnnouncementStoreGroup.class)
+    private String languageCode;
 
-  @NotNull(
-      message = "タイトルを入力してください",
-      groups = AnnouncementStoreGroup.class)
-  @Size(max = 256, message = "タイトルは256文字以内で入力してください", groups = AnnouncementStoreGroup.class)
-  private String title;
+    @NotBlank(
+            message = "タイトルを入力してください",
+            groups = AnnouncementStoreGroup.class)
+    @Size(max = 256, message = "タイトルは256文字以内で入力してください", groups = AnnouncementStoreGroup.class)
+    private String title;
 
-  @NotNull(
-      message = "表示メッセージを入力してください",
-      groups = AnnouncementStoreGroup.class)
-  @Size(max = 512, message = "表示メッセージは512文字以内で入力してください", groups = AnnouncementStoreGroup.class)
-  private String message;
+    @NotBlank(
+            message = "表示メッセージを入力してください",
+            groups = AnnouncementStoreGroup.class)
+    @Size(max = 512, message = "表示メッセージは512文字以内で入力してください", groups = AnnouncementStoreGroup.class)
+    private String message;
 
-  @Size(
-      max = 1024,
-      message = "リンク先 URL は1024文字で入力してください",
-      groups = AnnouncementStoreGroup.class)
-  @URL(message = "リンク先 URL はURLの形式で入力してください", groups = AnnouncementStoreGroup.class)
-  private String linkUrl;
+    @Size(
+            max = 1024,
+            message = "リンク先 URL は1024文字で入力してください",
+            groups = AnnouncementStoreGroup.class)
+    @URL(message = "リンク先 URL はURLの形式で入力してください", groups = AnnouncementStoreGroup.class)
+    private String linkUrl;
 }
