61e105df9ac76a10673acc69779975dcdf976e2f
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2025-09-02 11:59:02 +0900

    国際化: 日本語のエラーメッセージを追加し、messages_ja.propertiesを新規作成。プロパティファイルの設定を整理。

--- samples/cms/web/src/main/java/com/dressca/cms/web/WebApplication.java
+++ samples/cms/web/src/main/java/com/dressca/cms/web/WebApplication.java
@@ -8,9 +8,8 @@ import io.swagger.v3.oas.annotations.info.Info;
 /**
  * CMS アプリケーションのアプリケーションクラスです。
  */
-@SpringBootApplication(scanBasePackages = {"com.dressca.cms"})
-@OpenAPIDefinition(
-    info = @Info(title = "Dressca CMS", description = "Dressca CMS", version = "v1"))
+@SpringBootApplication(scanBasePackages = { "com.dressca.cms" })
+@OpenAPIDefinition(info = @Info(title = "Dressca CMS", description = "Dressca CMS", version = "v1"))
 public class WebApplication {
   public static void main(String[] args) {
     SpringApplication.run(WebApplication.class, args);
