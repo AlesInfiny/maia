2b25508875ddf66edefbb6892b5fa4be0f4cae58
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-10-20 16:04:40 +0900

    id にも autocomplete 属性を付与

--- samples/web-csr/dressca-frontend/consumer/src/views/authentication/LoginView.vue
+++ samples/web-csr/dressca-frontend/consumer/src/views/authentication/LoginView.vue
@@ -56,6 +56,7 @@ const signInOnClick = () => {
             v-model="email"
             type="text"
             placeholder="email"
+            autocomplete="username"
             class="w-full border-b px-4 py-2 placeholder-gray-500/50 focus:border-b-2 focus:border-indigo-500 focus:outline-hidden"
           />
         </div>
