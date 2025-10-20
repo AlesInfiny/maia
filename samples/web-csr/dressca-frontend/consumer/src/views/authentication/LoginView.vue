ec35dc4ab1914d356d32ad7be16b42fd73746ef5
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-10-20 17:20:14 +0900

    ブラウザから開発者コンソールに出力される警告2件に対処 (#3262)
    * input タグに autocomplete 属性を付与
    
    * イベントリスナーを passive に変更し、CSS でブラウザのオートスクロールをy軸方向のみ許可
    
    * id にも autocomplete 属性を付与

--- samples/web-csr/dressca-frontend/consumer/src/views/authentication/LoginView.vue
+++ samples/web-csr/dressca-frontend/consumer/src/views/authentication/LoginView.vue
@@ -56,6 +56,7 @@ const signInOnClick = () => {
             v-model="email"
             type="text"
             placeholder="email"
+            autocomplete="username"
             class="w-full border-b px-4 py-2 placeholder-gray-500/50 focus:border-b-2 focus:border-indigo-500 focus:outline-hidden"
           />
         </div>
@@ -69,6 +70,7 @@ const signInOnClick = () => {
             v-model="password"
             type="password"
             placeholder="password"
+            autocomplete="current-password"
             class="w-full border-b px-4 py-2 placeholder-gray-500/50 focus:border-b-2 focus:border-indigo-500 focus:outline-hidden"
           />
         </div>
