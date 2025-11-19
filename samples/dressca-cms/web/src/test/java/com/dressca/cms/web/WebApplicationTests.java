6e9ee84826914b7c515e43d791e33232a1e77326
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2025-11-19 10:12:53 +0900

    Webアプリの起動クラスのテストにコメントを追加

--- samples/dressca-cms/web/src/test/java/com/dressca/cms/web/WebApplicationTests.java
+++ samples/dressca-cms/web/src/test/java/com/dressca/cms/web/WebApplicationTests.java
@@ -1,11 +1,17 @@
 package com.dressca.cms.web;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * {@link WebApplication} の動作をテストするクラスです。
+ */
 @SpringBootTest
 class WebApplicationTests {
   @Test
+  @DisplayName("コンテキストが正常に読み込まれるか確認")
   void contextLoads() {
+
   }
 }
