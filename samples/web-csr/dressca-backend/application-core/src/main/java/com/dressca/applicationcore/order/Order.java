d211042aac60b258a296df8860e037b8568e71c6
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2025-12-25 14:24:15 +0900

    orderを修正

--- samples/web-csr/dressca-backend/application-core/src/main/java/com/dressca/applicationcore/order/Order.java
+++ samples/web-csr/dressca-backend/application-core/src/main/java/com/dressca/applicationcore/order/Order.java
@@ -33,9 +33,9 @@ public class Order {
   /**
    * {@link Order} クラスのインスタンスを初期化します。
    * 
-   * @param buyerId       購入者 ID 。
+   * @param buyerId 購入者 ID 。
    * @param shipToAddress 宛先住所。
-   * @param orderItems    商品リスト。
+   * @param orderItems 商品リスト。
    */
   public Order(String buyerId, ShipTo shipToAddress, List<OrderItem> orderItems) {
     this.buyerId = buyerId;
@@ -54,16 +54,16 @@ public class Order {
   /**
    * {@link Order} クラスのインスタンスを初期化します。
    * 
-   * @param id                 ID 。
-   * @param buyerId            購入者 ID 。
-   * @param orderDate          注文日付。
-   * @param shipToAddress      宛先住所。
+   * @param id ID 。
+   * @param buyerId 購入者 ID 。
+   * @param orderDate 注文日付。
+   * @param shipToAddress 宛先住所。
    * @param consumptionTaxRate 消費税率。
-   * @param totalItemsPrice    商品価格合計。
-   * @param deliveryCharge     送料。
-   * @param consumptionTax     消費税額。
-   * @param totalPrice         合計料金。
-   * @param orderItems         商品リスト。
+   * @param totalItemsPrice 商品価格合計。
+   * @param deliveryCharge 送料。
+   * @param consumptionTax 消費税額。
+   * @param totalPrice 合計料金。
+   * @param orderItems 商品リスト。
    */
   public Order(long id, String buyerId, LocalDateTime orderDate, ShipTo shipToAddress,
       BigDecimal consumptionTaxRate, BigDecimal totalItemsPrice, BigDecimal deliveryCharge,
@@ -72,9 +72,9 @@ public class Order {
     this.buyerId = buyerId;
     this.shipToAddress = shipToAddress;
     this.orderItems = orderItems;
-    this.account = new Account(
-        orderItems.stream().map(item -> new AccountItem(item.getQuantity(), item.getUnitPrice()))
-            .collect(Collectors.toList()));
+    this.account = new Account(orderItems.stream()
+        .map(item -> new AccountItem(item.getQuantity(), item.getUnitPrice()))
+        .collect(Collectors.toList()));
     this.consumptionTaxRate = Account.CONSUMPTION_TAX_RATE;
     this.totalItemsPrice = this.account.getItemTotalPrice();
     this.deliveryCharge = this.account.getDeliveryCharge();
