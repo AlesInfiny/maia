8f061b8d6053232e4ed887c838d6bbdbb1ae81aa
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2026-01-14 16:09:11 +0900

    共通の例外 ID 定数を追加し、エラーメッセージを定義しました。

--- samples/dressca-cms/system-common/src/main/java/com/dressca/cms/systemcommon/constant/CommonExceptionIdConstants.java
+++ samples/dressca-cms/system-common/src/main/java/com/dressca/cms/systemcommon/constant/CommonExceptionIdConstants.java
@@ -4,6 +4,9 @@ package com.dressca.cms.systemcommon.constant;
  * 共通の例外 ID 定数クラスです。
  */
 public class CommonExceptionIdConstants {
-  /** システムエラーの例外 ID です。 */
-  public static final String SYSTEM_ERROR = "E90001";
+  /** 想定外のシステムエラーが発生しました。 */
+  public static final String E_SYSTEM = "systemError";
+
+  /** 想定外の業務エラーが発生しました。 */
+  public static final String E_BUSINESS = "businessError";
 }
