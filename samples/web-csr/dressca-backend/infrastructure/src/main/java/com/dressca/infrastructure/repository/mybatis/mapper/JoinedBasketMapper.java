ee8bc2d0f9394ba590629ebb813fb15202c52579
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2026-01-19 16:41:35 +0900

    インフラ層のCHeckstyleの警告に対応

--- samples/web-csr/dressca-backend/infrastructure/src/main/java/com/dressca/infrastructure/repository/mybatis/mapper/JoinedBasketMapper.java
+++ samples/web-csr/dressca-backend/infrastructure/src/main/java/com/dressca/infrastructure/repository/mybatis/mapper/JoinedBasketMapper.java
@@ -9,8 +9,19 @@ import org.apache.ibatis.annotations.Param;
  */
 @Mapper
 public interface JoinedBasketMapper {
-
+  /**
+   * 指定した ID の買い物かごを取得します。
+   *
+   * @param id 買い物かご ID 。
+   * @return 条件に一致する買い物かご。
+   */
   Basket findById(@Param("id") long id);
 
+  /**
+   * 指定した購入者 ID の買い物かごを取得します。
+   * 
+   * @param buyerId 購入者 ID 。
+   * @return 条件に一致する買い物かご。
+   */
   Basket findByBuyerId(@Param("buyerId") String buyerId);
 }
