c411d7e27ee57b21a829d4aa54cd101e9634e4e5
rnakagawa16 <144891521+rnakagawa16@users.noreply.github.com>
2026-01-21 14:26:21 +0900

    Microsoft Entra External ID サンプルを作成する (#3731)
    * external id 用プロジェクトをコピー
    
    * サンプルの編集
    
    * サンプル実装
    
    * external id 用の修正
    
    * 指摘修正
    
    * READMEの修正
    
    * バージョン情報の削除
    
    * README修正
    
    * フォルダー名修正
    
    * 修正
    
    * 指摘事項の修正
    
    * SpringBoot4対応
    
    * 環境変数取得の変更
    
    * ライブラリ変更と軽微な修正
    
    * npm パッケージアップデート
    
    * nodeバージョンを修正
    
    * README修正
    
    * ドキュメントに合わせた修正
    
    * 静的解析の設定追加
    
    * READMEの修正
    
    * Genarate Client の再生成
    
    * フロントエンド修正
    
    * Lint対応
    
    * formatter の適用
    
    * SecurityFilterChainを Dressca と合わせる
    
    * 重複削除
    
    * フロントエンド、バックエンドの指摘対応
    
    * ドキュメント修正
    
    * 指摘対応
    
    * cypress関連ファイル削除
    
    * Cypressの修正
    
    * 修正
    
    * READMEの修正
    
    * ドキュメント修正
    
    * Node.js のセキュリティパッチ適用
    
    * 修正
    
    * 修正
    
    * ドキュメント修正
    
    ---------
    
    Co-authored-by: Nakagawa Ryo <rnakagawa3+biprogy@ixas.unisys.co.jp>
    Co-authored-by: Nakagawa Ryo <rnakagawa3+bipsemi0@AlesLab.onmicrosoft.com>
    Co-authored-by: NakagawaRyo0616 <edge.dubber@gmail.com>

---
+++ samples/external-id-sample-for-spa/auth-backend/system-common/src/main/java/com/dressca/systemcommon/log/AbstractStructuredLogger.java
@@ -0,0 +1,140 @@
+package com.dressca.systemcommon.log;
+
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.dressca.systemcommon.constant.SystemPropertyConstants;
+
+/**
+ * コンテキストにログを保持させる機能を追加した構造化ロガーのインターフェースです。
+ */
+public abstract class AbstractStructuredLogger {
+
+  private static final Logger apLog =
+      LoggerFactory.getLogger(SystemPropertyConstants.APPLICATION_LOG_LOGGER);
+
+  /**
+   * 構造化ログでデフォルトで出力する内容を追加します。 try ブロック内に MDC.put(key, value) で追記してください。
+   * 
+   * @param logAction ログを出力するメソッド。
+   */
+  protected abstract void logWithMdc(Runnable logAction);
+
+  /**
+   * DEBUG レベルのログを出力します。
+   * 
+   * @param msg ログのメッセージ。
+   */
+  public void debug(String msg) {
+    logWithMdc(() -> apLog.debug(msg));
+  }
+
+  /**
+   * Key Value を追加して DEBUG レベルのログを出力します。
+   * 
+   * @param msg ログのメッセージ。
+   * @param keyValueMap Key Value のマップ。
+   */
+  public void debug(String msg, Map<String, String> keyValueMap) {
+    logWithMdc(() -> {
+      var logBuilder = apLog.atDebug().setMessage(msg);
+      keyValueMap.forEach(logBuilder::addKeyValue);
+      logBuilder.log();
+    });
+  }
+
+  /**
+   * ERROR レベルのログを出力します。
+   * 
+   * @param msg ログのメッセージ。
+   */
+  public void error(String msg) {
+    logWithMdc(() -> apLog.error(msg));
+  }
+
+  /**
+   * Key Value を追加して ERROR レベルのログを出力します。
+   * 
+   * @param msg ログのメッセージ。
+   * @param keyValueMap Key Value のマップ。
+   */
+  public void error(String msg, Map<String, String> keyValueMap) {
+
+    logWithMdc(() -> {
+      var logBuilder = apLog.atError().setMessage(msg);
+      keyValueMap.forEach(logBuilder::addKeyValue);
+      logBuilder.log();
+    });
+  }
+
+  /**
+   * INFO レベルのログを出力します。
+   * 
+   * @param msg ログのメッセージ。
+   */
+  public void info(String msg) {
+    logWithMdc(() -> apLog.info(msg));
+  }
+
+  /**
+   * Key Value を追加して INFO レベルのログを出力します。
+   * 
+   * @param msg ログのメッセージ。
+   * @param keyValueMap Key Value のマップ。
+   */
+  public void info(String msg, Map<String, String> keyValueMap) {
+    logWithMdc(() -> {
+      var logBuilder = apLog.atInfo().setMessage(msg);
+      keyValueMap.forEach(logBuilder::addKeyValue);
+      logBuilder.log();
+    });
+  }
+
+  /**
+   * TRACE レベルのログを出力します。
+   * 
+   * @param msg ログのメッセージ。
+   */
+  public void trace(String msg) {
+    logWithMdc(() -> apLog.trace(msg));
+  }
+
+  /**
+   * Key Value を追加して TRACE レベルのログを出力します。
+   * 
+   * @param msg ログのメッセージ。
+   * @param keyValueMap Key Value のマップ。
+   */
+  public void trace(String msg, Map<String, String> keyValueMap) {
+    logWithMdc(() -> {
+      var logBuilder = apLog.atTrace().setMessage(msg);
+      keyValueMap.forEach(logBuilder::addKeyValue);
+      logBuilder.log();
+    });
+  }
+
+  /**
+   * WARN レベルのログを出力します。
+   * 
+   * @param msg ログのメッセージ。
+   */
+  public void warn(String msg) {
+
+    logWithMdc(() -> apLog.warn(msg));
+  }
+
+  /**
+   * Key Value を追加して WARN レベルのログを出力します。
+   * 
+   * @param msg ログのメッセージ。
+   * @param keyValueMap Key Value のマップ。
+   */
+  public void warn(String msg, Map<String, String> keyValueMap) {
+    logWithMdc(() -> {
+      var logBuilder = apLog.atWarn().setMessage(msg);
+      keyValueMap.forEach(logBuilder::addKeyValue);
+      logBuilder.log();
+    });
+  }
+
+}
