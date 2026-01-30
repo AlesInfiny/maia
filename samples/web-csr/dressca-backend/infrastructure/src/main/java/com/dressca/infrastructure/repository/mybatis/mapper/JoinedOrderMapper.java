db1d3b1e5b6948b1517b2d83282bbce00456687b
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2026-01-30 15:14:40 +0900

    DresscaのCheckstyleの設定ファイルを最新化

--- samples/web-csr/dressca-backend/infrastructure/src/main/java/com/dressca/infrastructure/repository/mybatis/mapper/JoinedOrderMapper.java
+++ samples/web-csr/dressca-backend/infrastructure/src/main/java/com/dressca/infrastructure/repository/mybatis/mapper/JoinedOrderMapper.java
@@ -11,10 +11,26 @@ import org.apache.ibatis.annotations.Param;
  */
 @Mapper
 public interface JoinedOrderMapper {
-
+  /**
+   * 注文を追加します。
+   * 
+   * @param order 注文。
+   */
   void add(@Param("order") Order order);
 
+  /**
+   * 注文商品を追加します。
+   * 
+   * @param orderId 注文 ID 。
+   * @param orderItems 注文商品リスト。
+   */
   void addItems(@Param("orderId") long orderId, @Param("orderItems") List<OrderItem> orderItems);
 
+  /**
+   * 指定した ID の注文情報を取得します。
+   * 
+   * @param id ID 。
+   * @return 注文情報。
+   */
   Order findById(@Param("id") long id);
 }
