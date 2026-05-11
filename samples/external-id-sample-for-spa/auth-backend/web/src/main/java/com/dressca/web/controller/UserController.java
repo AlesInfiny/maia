29012eb4faa73ae94f4c8f59660a0d3514abaf0e
fkoyama <fumika.koyama@biprogy.com>
2026-05-11 11:34:13 +0900

    ExternalIDサンプルのDTOをリネーム

--- samples/external-id-sample-for-spa/auth-backend/web/src/main/java/com/dressca/web/controller/UserController.java
+++ samples/external-id-sample-for-spa/auth-backend/web/src/main/java/com/dressca/web/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.dressca.web.controller.dto.auth.UserResponse;
+import com.dressca.web.controller.dto.auth.GetUserResponse;
 import com.dressca.web.security.UserIdThreadContextFilter;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -35,16 +35,16 @@ public class UserController {
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "成功。",
           content = @Content(mediaType = "application/json",
-              schema = @Schema(implementation = UserResponse.class))),
+              schema = @Schema(implementation = GetUserResponse.class))),
       @ApiResponse(responseCode = "401", description = "未認証エラー。",
           content = @Content(mediaType = "application/problem+json",
               schema = @Schema(implementation = ProblemDetail.class)))})
   @GetMapping
   @PreAuthorize(value = "isAuthenticated()")
-  public ResponseEntity<UserResponse> getUser() throws Exception {
+  public ResponseEntity<GetUserResponse> getUser() throws Exception {
 
     String userId = UserIdThreadContextFilter.threadLocalUserId.get();
-    UserResponse response = new UserResponse(userId);
+    GetUserResponse response = new GetUserResponse(userId);
     return ResponseEntity.ok().body(response);
   }
 
