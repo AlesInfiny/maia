cef7f2dd1894fc1c66e531de9453237b89076327
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2026-06-15 11:20:38 +0900

    掲載品の例外メッセージの定数を追加

--- samples/web-csr/dressca-backend/application-core/src/main/java/com/dressca/applicationcore/constant/ExceptionIdConstants.java
+++ samples/web-csr/dressca-backend/application-core/src/main/java/com/dressca/applicationcore/constant/ExceptionIdConstants.java
@@ -17,10 +17,6 @@ public class ExceptionIdConstants {
   /** 存在しない買い物かごID: {0} の買い物かごが要求されました。 */
   public static final String E_BASKET_IS_NULL_ON_CHECKOUT = "basketIsNullOnCheckout";
 
-  /** 買い物かごID: {0} に商品ID: {1} の商品が見つかりませんでした。 */
-  public static final String E_CATALOG_ITEM_ID_DOES_NOT_EXIST_IN_BASKET =
-      "catalogItemIdDoesNotExistInBasket";
-
   /** 注文のチェックアウト時に買い物かごが空でした。 */
   public static final String E_BASKET_IS_EMPTY_ON_CHECKOUT = "basketIsEmptyOnCheckout";
 
@@ -41,4 +37,11 @@ public class ExceptionIdConstants {
 
   /** カタログアイテム ID: {0} の {1} 実行時に楽観ロックエラーが発生しました。 */
   public static final String E_OPTIMISTIC_LOCKING_FAILURE = "optimisticLockingFailure";
+
+  /** 掲載品ID: {0} の掲載品が見つかりませんでした。 */
+  public static final String E_DISPLAY_ITEM_ID_NOT_FOUND = "displayItemIdNotFound";
+
+  /** 買い物かごID: {0} に掲載品ID: {1} の掲載品が見つかりませんでした。 */
+  public static final String E_DISPLAY_ITEM_ID_DOES_NOT_EXIST_IN_BASKET =
+      "displayItemIdDoesNotExistInBasket";
 }
