4017370d5d26e887d4cd013903f5b2cedb1dd70e
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2026-07-23 13:14:44 +0900

    フロントエンドの修正

---
+++ samples/web-csr/dressca-frontend/consumer/mock/data/catalog-brands.ts
@@ -0,0 +1,16 @@
+import type { GetDisplayBrandsResponse } from '@/generated/api-client'
+
+export const displayBrands: GetDisplayBrandsResponse[] = [
+  {
+    id: '019b76da-a800-7002-8001-000000000001',
+    name: '高級なブランド',
+  },
+  {
+    id: '019b76da-a800-7002-8001-000000000002',
+    name: 'カジュアルなブランド',
+  },
+  {
+    id: '019b76da-a800-7002-8001-000000000003',
+    name: 'ノーブランド',
+  },
+]
