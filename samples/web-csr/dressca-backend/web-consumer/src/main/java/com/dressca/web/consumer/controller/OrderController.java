79da41a74b9b55b8cb7c06e260d401aee5eca069
Nakagawa Ryo <rnakagawa3+biprogy@ixas.unisys.co.jp>
2026-07-27 14:01:38 +0900

    CheckStyleの警告解消

--- samples/web-csr/dressca-backend/web-consumer/src/main/java/com/dressca/web/consumer/controller/OrderController.java
+++ samples/web-csr/dressca-backend/web-consumer/src/main/java/com/dressca/web/consumer/controller/OrderController.java
@@ -61,8 +61,12 @@ public class OrderController {
    */
   @Operation(summary = "注文情報を取得します。", description = "注文情報を取得します。")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "成功。", content = @Content(mediaType = "application/json", schema = @Schema(implementation = GetOrderByIdResponse.class))),
-      @ApiResponse(responseCode = "404", description = "注文 ID が存在しません。", content = @Content(mediaType = "application/problem+json", schema = @Schema(implementation = ProblemDetail.class))) })
+      @ApiResponse(responseCode = "200", description = "成功。",
+          content = @Content(mediaType = "application/json",
+              schema = @Schema(implementation = GetOrderByIdResponse.class))),
+      @ApiResponse(responseCode = "404", description = "注文 ID が存在しません。",
+          content = @Content(mediaType = "application/problem+json",
+              schema = @Schema(implementation = ProblemDetail.class)))})
   @GetMapping("{orderId}")
   public ResponseEntity<?> getOrderById(@PathVariable("orderId") UUID orderId,
       HttpServletRequest req) {
@@ -91,9 +95,14 @@ public class OrderController {
    * @return なし。
    */
   @Operation(summary = "買い物かごに登録されている商品を注文します。", description = "買い物かごに登録されている商品を注文します。")
-  @ApiResponses(value = { @ApiResponse(responseCode = "201", description = "成功。", content = @Content),
-      @ApiResponse(responseCode = "400", description = "リクエストエラー。", content = @Content(mediaType = "application/problem+json", schema = @Schema(implementation = ProblemDetail.class))),
-      @ApiResponse(responseCode = "500", description = "サーバーエラー。", content = @Content(mediaType = "application/problem+json", schema = @Schema(implementation = ProblemDetail.class))) })
+  @ApiResponses(
+      value = {@ApiResponse(responseCode = "201", description = "成功。", content = @Content),
+          @ApiResponse(responseCode = "400", description = "リクエストエラー。",
+              content = @Content(mediaType = "application/problem+json",
+                  schema = @Schema(implementation = ProblemDetail.class))),
+          @ApiResponse(responseCode = "500", description = "サーバーエラー。",
+              content = @Content(mediaType = "application/problem+json",
+                  schema = @Schema(implementation = ProblemDetail.class)))})
   @PostMapping
   public ResponseEntity<?> postOrder(@RequestBody @Valid PostOrderRequest postOrderInput,
       HttpServletRequest req) {
