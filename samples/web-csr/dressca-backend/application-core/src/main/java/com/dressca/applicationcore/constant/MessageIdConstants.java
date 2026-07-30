42d12a53402f22e52785ced74e01fe3c4b2464c8
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2026-07-30 15:19:04 +0900

    メッセージの文言を修正

--- samples/web-csr/dressca-backend/application-core/src/main/java/com/dressca/applicationcore/constant/MessageIdConstants.java
+++ samples/web-csr/dressca-backend/application-core/src/main/java/com/dressca/applicationcore/constant/MessageIdConstants.java
@@ -8,72 +8,72 @@ public class MessageIdConstants {
   /** アセット情報{0}を取得します。 */
   public static final String D_ASSET_GET_ASSET = "assetApplicationServiceGetAsset";
 
-  /** 買い物かごに商品（顧客ID: {0}, カタログ商品ID: {1}, 数量: {2}）を追加します。 */
-  public static final String D_SHOPPING_ADD_ITEM_TO_BASKET =
-      "shoppingApplicationServiceAddItemToBasket";
-
-  /** 買い物かごの商品の数量（顧客ID: {0}, 数量: {1}）を設定します。 */
-  public static final String D_SHOPPING_SET_BASKET_ITEMS_QUANTITIES =
-      "shoppingApplicationServiceSetBasketItemsQuantities";
-
-  /** 顧客（顧客ID: {0}）の買い物かごから商品（カタログ商品ID: {1}）を削除します。 */
-  public static final String D_SHOPPING_DELETE_ITEM_FROM_BASKET =
-      "shoppingApplicationServiceDeleteItemFromBasket";
-
-  /** 顧客（顧客ID: {0}）の買い物かごと情報とその商品一覧を取得します。 */
-  public static final String D_SHOPPING_GET_BASKET_ITEMS =
-      "shoppingApplicationServiceGetBasketItems";
-
-  /** 注文（顧客ID: {0}, お届け先: {1}）を確定します。 */
-  public static final String D_SHOPPING_CHECKOUT = "shoppingApplicationServiceCheckout";
-
-  /** 指定した注文ID: {0}, 購入者ID: {1} の注文情報を取得します。 */
-  public static final String D_ORDER_GET_ORDER = "orderApplicationServiceGetOrder";
-
-  /** 条件（ブランドID: {0}, カテゴリID: {1}, ページ: {2}, ページサイズ: {3}）に一致するカタログ情報を取得します。 */
-  public static final String D_CATALOG_GET_CATALOG_ITEMS =
-      "catalogApplicationServiceGetCatalogItems";
+  /** カタログにアイテムを追加します。 */
+  public static final String D_CATALOG_ADD_ITEM_TO_CATALOG =
+      "catalogApplicationServiceAddItemToCatalog";
 
   /** 条件（ブランドID: {0}, カテゴリID: {1}）に一致するカテゴリの件数を取得します。 */
   public static final String D_CATALOG_COUNT_CATALOG_ITEMS =
       "catalogApplicationServiceCountCatalogItems";
 
+  /** カタログアイテム ID: {0} のカタログアイテムを削除します。 */
+  public static final String D_CATALOG_DELETE_ITEM_FROM_CATALOG =
+      "catalogApplicationServiceDeleteItemFromCatalog";
+
   /** フィルタリング用のカタログブランドリストを取得します。 */
   public static final String D_CATALOG_GET_BRANDS = "catalogApplicationServiceGetBrands";
-
-  /** フィルタリング用のカタログカテゴリリストを取得します。 */
-  public static final String D_CATALOG_GET_CATEGORIES = "catalogApplicationServiceGetCategories";
 
   /** カタログアイテム ID: {0} のカタログアイテムを取得します。 */
   public static final String D_CATALOG_GET_CATALOG_ITEM = "catalogApplicationServiceGetCatalogItem";
 
-  /** カタログにアイテムを追加します。 */
-  public static final String D_CATALOG_ADD_ITEM_TO_CATALOG =
-      "catalogApplicationServiceAddItemToCatalog";
+  /** 条件（ブランドID: {0}, カテゴリID: {1}, ページ: {2}, ページサイズ: {3}）に一致するカタログ情報を取得します。 */
+  public static final String D_CATALOG_GET_CATALOG_ITEMS =
+      "catalogApplicationServiceGetCatalogItems";
 
-  /** カタログアイテム ID: {0} のカタログアイテムを削除します。 */
-  public static final String D_CATALOG_DELETE_ITEM_FROM_CATALOG =
-      "catalogApplicationServiceDeleteItemFromCatalog";
+  /** フィルタリング用のカタログカテゴリリストを取得します。 */
+  public static final String D_CATALOG_GET_CATEGORIES = "catalogApplicationServiceGetCategories";
 
   /** カタログアイテム ID: {0} のカタログアイテムを更新します。 */
   public static final String D_CATALOG_UPDATE_CATALOG_ITEM =
       "catalogApplicationServiceUpdateCatalogItem";
 
-  /** 条件（ブランドID: {0}, カテゴリID: {1}, ページ: {2}, ページサイズ: {3}）に一致する陳列品情報を取得します。 */
-  public static final String D_DISPLAY_GET_DISPLAY_ITEMS =
-      "displayApplicationServiceGetDisplayItems";
-
   /** 条件（ブランドID: {0}, カテゴリID: {1}）に一致する陳列品の件数を取得します。 */
   public static final String D_DISPLAY_COUNT_DISPLAY_ITEMS =
       "displayApplicationServiceCountDisplayItems";
-
-  /** 陳列品 ID 一覧: {0} に一致する陳列品情報を取得します。 */
-  public static final String D_DISPLAY_GET_DISPLAY_ITEMS_BY_IDS =
-      "displayApplicationServiceGetDisplayItemsByIds";
 
   /** フィルタリング用の陳列ブランドリストを取得します。 */
   public static final String D_DISPLAY_GET_BRANDS = "displayApplicationServiceGetBrands";
 
   /** フィルタリング用の陳列カテゴリリストを取得します。 */
   public static final String D_DISPLAY_GET_CATEGORIES = "displayApplicationServiceGetCategories";
+
+  /** 条件（ブランドID: {0}, カテゴリID: {1}, ページ: {2}, ページサイズ: {3}）に一致する陳列品情報を取得します。 */
+  public static final String D_DISPLAY_GET_DISPLAY_ITEMS =
+      "displayApplicationServiceGetDisplayItems";
+
+  /** 陳列品 ID 一覧: {0} に一致する陳列品情報を取得します。 */
+  public static final String D_DISPLAY_GET_DISPLAY_ITEMS_BY_IDS =
+      "displayApplicationServiceGetDisplayItemsByIds";
+
+  /** 指定した注文ID: {0}, 購入者ID: {1} の注文情報を取得します。 */
+  public static final String D_ORDER_GET_ORDER = "orderApplicationServiceGetOrder";
+
+  /** 買い物かごに陳列品（顧客ID: {0}, 陳列品ID: {1}, 数量: {2}）を追加します。 */
+  public static final String D_SHOPPING_ADD_ITEM_TO_BASKET =
+      "shoppingApplicationServiceAddItemToBasket";
+
+  /** 注文（顧客ID: {0}, お届け先: {1}）を確定します。 */
+  public static final String D_SHOPPING_CHECKOUT = "shoppingApplicationServiceCheckout";
+
+  /** 顧客（顧客ID: {0}）の買い物かごから陳列品（陳列品ID: {1}）を削除します。 */
+  public static final String D_SHOPPING_DELETE_ITEM_FROM_BASKET =
+      "shoppingApplicationServiceDeleteItemFromBasket";
+
+  /** 顧客（顧客ID: {0}）の買い物かご情報とその陳列品一覧を取得します。 */
+  public static final String D_SHOPPING_GET_BASKET_ITEMS =
+      "shoppingApplicationServiceGetBasketItems";
+
+  /** 買い物かごの陳列品の数量（顧客ID: {0}, 数量: {1}）を設定します。 */
+  public static final String D_SHOPPING_SET_BASKET_ITEMS_QUANTITIES =
+      "shoppingApplicationServiceSetBasketItemsQuantities";
 }
