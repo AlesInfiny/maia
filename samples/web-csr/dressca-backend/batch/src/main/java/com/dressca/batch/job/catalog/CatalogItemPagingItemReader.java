f46847333d435e3df55fe8d093e3befd0ae49dd1
yoshidakenji <181298858+kenjiyoshid-a@users.noreply.github.com>
2026-08-24 10:13:22 +0900

    不要な記載を削除

--- samples/web-csr/dressca-backend/batch/src/main/java/com/dressca/batch/job/catalog/CatalogItemPagingItemReader.java
+++ samples/web-csr/dressca-backend/batch/src/main/java/com/dressca/batch/job/catalog/CatalogItemPagingItemReader.java
@@ -4,25 +4,16 @@ import com.dressca.applicationmodules.catalogmanagement.CatalogApplicationServic
 import com.dressca.applicationmodules.catalogmanagement.entity.CatalogItem;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.infrastructure.item.database.AbstractPagingItemReader;
-import org.springframework.util.ClassUtils;
 
 /**
  * カタログ管理コンテキストの公開 API を利用して、 CatalogItem をページング単位で読み取る Reader です。
  */
+@RequiredArgsConstructor
 public class CatalogItemPagingItemReader extends AbstractPagingItemReader<CatalogItem> {
 
   private final CatalogApplicationService catalogApplicationService;
-
-  /**
-   * {@link CatalogItemPagingItemReader} クラスの新しいインスタンスを初期化します。
-   * 
-   * @param catalogApplicationService カタログアプリケーションサービス。
-   */
-  public CatalogItemPagingItemReader(CatalogApplicationService catalogApplicationService) {
-    this.catalogApplicationService = catalogApplicationService;
-    setName(ClassUtils.getShortName(CatalogItemPagingItemReader.class));
-  }
 
   @Override
   protected void doReadPage() {
