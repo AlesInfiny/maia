882a1b2a1154a2fa036d1ec9f2d8b8d84af838a0
Nakagawa Ryo <rnakagawa3+biprogy@ixas.unisys.co.jp>
2026-09-18 14:21:20 +0900

    importの修正

--- samples/dressca-cms/web/src/main/java/com/dressca/cms/web/models/base/AnnouncementContentViewModel.java
+++ samples/dressca-cms/web/src/main/java/com/dressca/cms/web/models/base/AnnouncementContentViewModel.java
@@ -2,9 +2,9 @@ package com.dressca.cms.web.models.base;
 
 import com.dressca.cms.web.models.validation.AnnouncementValidationGroup;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.util.UUID;
-import java.util.regex.Pattern;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
